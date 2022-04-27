96b8243e0dc6450fe800d31a45559bafccda42de
brenekv.03 <brenekv.03@A321-15>
2022-04-27 09:07:31 +0200

    Default

---
+++ OOP00_Dedicnost/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP00_Dedicnost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zamestnanec zamestnanec = new Zamestnanec("Eva", "Nováková", 100);
+            MessageBox.Show(zamestnanec.GetJmeno());
+            MessageBox.Show(zamestnanec.ToString());
+
+            zamestnanec.SetPrijmeni("Pospíchalová");
+            MessageBox.Show(zamestnanec.ToString());
+
+            zamestnanec.OdpracujHodiny(20);
+            MessageBox.Show(zamestnanec.ToString());
+            zamestnanec.OdpracujHodiny(150);
+            MessageBox.Show(zamestnanec.ToString());
+        }
+    }
+}
---
+++ OOP00_Dedicnost/Zamestnanec.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP00_Dedicnost
+{
+    class Zamestnanec
+    {
+        private string jmeno;
+        private string prijmeni;
+        private int odpracHodiny = 0;
+        public double HodinovaMzda { get; }
+        public Zamestnanec(string jmeno, string prijmeni, double hodinovaMzda)
+        {
+            this.jmeno = jmeno;
+            this.prijmeni = prijmeni;
+            HodinovaMzda = hodinovaMzda;
+        }
+        public string GetJmeno()
+        {
+            return jmeno;
+        }
+        public string GetPrijmeni()
+        {
+            return prijmeni;
+        }
+        public void SetPrijmeni (string prijmeni)
+        {
+            this.prijmeni = prijmeni;
+        }
+        public void OdpracujHodiny(int hodiny)
+        {
+            odpracHodiny += hodiny;
+        }
+        public double VypoctiMzdu()
+        {
+            return odpracHodiny * HodinovaMzda;
+        }
+        public bool Bonusy ()
+        {
+            return odpracHodiny >= 160;
+        }
+        public override string ToString()
+        {
+            string maBonus = "NE (Nemá nárok)";
+            if (Bonusy()) maBonus = "ANO (Má nárok)";
+            string s = "Jméno: " + jmeno
+                     + "\nPříjmení: " + prijmeni
+                     + "\nOdpracované hodiny: " + odpracHodiny
+                     + "\nHodinová mzda: " + HodinovaMzda
+                     + "\nVypočtená mzda: " + VypoctiMzdu()
+                     + "\nZaměstnanecké bonusy: " + maBonus;
+            return s;
+        }
+    }
+}
